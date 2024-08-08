48cacbdd2d88501ed2f581ac40f1b4340a7eec23
Fernando Díaz <fdsolinf@gmail.com>
2024-08-08 17:50:14 -0500

    Formularios de actualización de experiencia y academico ya se visualizan y se editan generando mensaje.

--- applications/candidato/views/CandidatoView.py
+++ applications/candidato/views/CandidatoView.py
@@ -105,6 +105,8 @@ def candidato_mostrar(request, pk=None):
         'estudios': estudios
         })
 
+global_dato = None 
+
 def obtener_estudio_view(request):
     global global_dato
 
@@ -112,10 +114,11 @@ def obtener_estudio_view(request):
         dato              = request.GET.get('dato')
         solicitud_estudio = get_object_or_404(Can103Educacion, pk=dato)
         global_dato = dato
+        
 
-        estado_id = Cat001Estado.objects.get(nombre=solicitud_estudio.estado_id_001)
+        estado_id    = Cat001Estado.objects.get(nombre=solicitud_estudio.estado_id_001)
         candidato_id = Can101Candidato.objects.get(email=solicitud_estudio.candidato_id_101)
-        ciudad_id = Cat004Ciudad.objects.get(nombre=solicitud_estudio.ciudad_id_004)
+        ciudad_id    = Cat004Ciudad.objects.get(nombre=solicitud_estudio.ciudad_id_004)
     
         response_data = {
             'data': {
@@ -136,7 +139,6 @@ def obtener_estudio_view(request):
         return JsonResponse(response_data)
     
     if request.method == 'POST':
-        estado_id_001 = None
         institucion = None
         fecha_inicial = None
         fecha_final = None
@@ -145,34 +147,111 @@ def obtener_estudio_view(request):
         fortaleza_adquiridas = None
         candidato_id_101 = None
         ciudad_id_004 = None
+        grado_en = None
 
-        estado_id_001 = request.POST.get('estado_id_001')
         institucion = request.POST.get('institucion')
         fecha_inicial = request.POST.get('fecha_inicial')
         fecha_final = request.POST.get('fecha_final')
         titulo = request.POST.get('titulo')
         carrera = request.POST.get('carrera')
         fortaleza_adquiridas = request.POST.get('fortaleza_adquiridas')
-        candidato_id_101 = request.POST.get('candidato_id_101')
+        candidato_id_101 = request.POST.get('id_candidato')
         ciudad_id_004 = request.POST.get('ciudad_id_004')
-
+        grado_en = request.POST.get('grado_en')
+        
         ghost = global_dato
+
+        
         
         solicitud_estudio_modificar = get_object_or_404(Can103Educacion, pk=ghost)
+        # Obtener la instancia del modelo Can101Candidato
+        candidato = get_object_or_404(Can101Candidato, pk=candidato_id_101)
 
-        solicitud_estudio_modificar.estado_id_001 = estado_id_001
+        # Obtener la instancia del modelo Cat004Ciudad
+        ciudad = get_object_or_404(Cat004Ciudad, pk=ciudad_id_004)
+
+        solicitud_estudio_modificar.estado_id_001 = Cat001Estado.objects.get(id=1)
         solicitud_estudio_modificar.institucion = institucion
         solicitud_estudio_modificar.fecha_inicial = fecha_inicial
         solicitud_estudio_modificar.fecha_final = fecha_final
         solicitud_estudio_modificar.titulo = titulo
         solicitud_estudio_modificar.carrera = carrera
         solicitud_estudio_modificar.fortaleza_adquiridas = fortaleza_adquiridas
-        solicitud_estudio_modificar.candidato_id_101 = candidato_id_101
-        solicitud_estudio_modificar.ciudad_id_004 = ciudad_id_004
-        
-        solicitud_estudio_modificar.save()
-        print(estado_id)
-        messages.success(request, 'La solicitud de Vacaciones/Licencias ha sido actualizada correctamente.')
-        return redirect('candidatos:candidato_editar', pk=solicitud_estudio_modificar.id)
+        solicitud_estudio_modificar.ciudad_id_004 = ciudad
+        solicitud_estudio_modificar.grado_en = grado_en
 
+        solicitud_estudio_modificar.save()
+
+        messages.success(request, 'Se ha realizado la actualización del registro éxito.')
+        return redirect('candidatos:candidato_editar', pk=candidato.id)
+
+    return JsonResponse({'error': 'Método no permitido'}, status=405)
+
+# 
+global_id_laboral = None
+def obtener_laboral_view(request):
+    global global_id_laboral
+
+    if request.method == 'GET':
+        dato              = request.GET.get('dato')
+        solicitud_laboral = get_object_or_404(Can102Experiencia, pk=dato)
+        global_id_laboral = dato
+
+        estado_id    = Cat001Estado.objects.get(nombre=solicitud_laboral.estado_id_001)
+        candidato_id = Can101Candidato.objects.get(email=solicitud_laboral.candidato_id_101)
+
+        print(solicitud_laboral.sector)
+        response_data = {
+            'data': {
+                'id': solicitud_laboral.id,
+                'estado_id_001': estado_id.id,
+                'entidad': solicitud_laboral.entidad,
+                'sector': solicitud_laboral.sector,
+                'fecha_inicial': solicitud_laboral.fecha_inicial,
+                'fecha_final': solicitud_laboral.fecha_final,
+                'activo': solicitud_laboral.activo,
+                'logro': solicitud_laboral.logro,
+                'candidato_id_101': candidato_id.id,
+                'cargo': solicitud_laboral.cargo,
+            }
+        }
+        
+        return JsonResponse(response_data)
+    
+    if request.method == 'POST':
+        entidad = None
+        sector = None
+        activo = None
+        fecha_inicial = None
+        fecha_final = None
+        cargo = None
+
+        entidad = request.POST.get('entidad')
+        sector = request.POST.get('sector')
+        activo = request.POST.get('activo')
+        fecha_inicial = request.POST.get('fecha_inicial')
+        fecha_final = request.POST.get('fecha_final')
+        cargo = request.POST.get('cargo')
+        candidato_id_101 = request.POST.get('id_candidato')
+
+        id_experiencia = global_id_laboral
+
+        solicitud_laboral_modificar = get_object_or_404(Can102Experiencia, pk=id_experiencia)
+
+        # Obtener la instancia del modelo Can101Candidato
+        candidato = get_object_or_404(Can101Candidato, pk=candidato_id_101)
+
+        solicitud_laboral_modificar.estado_id_001 = Cat001Estado.objects.get(id=1)
+        solicitud_laboral_modificar.entidad = entidad
+        solicitud_laboral_modificar.sector = sector
+        solicitud_laboral_modificar.activo = activo
+        solicitud_laboral_modificar.fecha_inicial = fecha_inicial
+        solicitud_laboral_modificar.fecha_final = fecha_final
+        solicitud_laboral_modificar.cargo = cargo
+
+        solicitud_laboral_modificar.save()
+    
+        messages.success(request, 'Se ha realizado la actualización del registro éxito.')
+        return redirect('candidatos:candidato_editar', pk=candidato.id)
+    
     return JsonResponse({'error': 'Método no permitido'}, status=405)
