9d82f16f4ef281572803b232ef34d942722fd03f
Fernando Díaz <fdsolinf@gmail.com>
2024-09-03 21:54:11 -0500

    Actualización Formulario de Habilidades en pruebas

--- applications/common/urls.py
+++ applications/common/urls.py
@@ -5,5 +5,7 @@ url_principal = 'pruebas/'
 
 urlpatterns = [
     path( url_principal+'listado_simple', PruebasView.ListadoSimpleListView.as_view(), name='listado'),
-    path( url_principal+'listado_exportar', PruebasView.ListadoExportarListView.as_view(), name='listado')
+    path( url_principal+'listado_exportar', PruebasView.ListadoExportarListView.as_view(), name='listado'),
+    path( url_principal+'texto_sugerido', PruebasView.PruebaTextoSugerido.as_view(), name='listado'),
+    path( url_principal+'api/prueba_texto_sugerido', PruebasView.sugerir_habilidades, name='habilidad_sugerida')
 ]
--- applications/common/views/PruebasView.py
+++ applications/common/views/PruebasView.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from applications.candidato.models import Can101Candidato
+from applications.candidato.models import Can101Candidato, Can104Skill
 from django.views import View
+from django.http import JsonResponse
+from django.db.models import Q
 
 class ListadoSimpleListView(View):
     model = Can101Candidato
@@ -23,3 +25,20 @@ class ListadoExportarListView(View):
         
         # Renderizar la plantilla con los datos
         return render(request, self.template_name, {'candidatos': candidatos})
+    
+class PruebaTextoSugerido(View):
+    model = Can101Candidato
+    template_name = "pruebas/form_auto_completado.html"
+
+    def get(self, request, *args, **kwargs):
+        # Obtener todos los objetos del modelo
+        candidatos = self.model.objects.all()
+        
+        # Renderizar la plantilla con los datos
+        return render(request, self.template_name, {'candidatos': candidatos})
+
+def sugerir_habilidades(request):
+    query = request.GET.get('query', '')
+    habilidades = Can104Skill.objects.filter(nombre__icontains=query)[:5]
+    sugerencias = [habilidad.nombre for habilidad in habilidades]
+    return JsonResponse(sugerencias, safe=False)
