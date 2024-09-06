24dfe91f8a0060c5a16164c474af447250c88167
Fernando Díaz <fdsolinf@gmail.com>
2024-09-05 23:01:50 -0500

    Modelo actualizado de vacantes y dependencias

--- applications/vacante/models.py
+++ applications/vacante/models.py
@@ -2,40 +2,42 @@ from django.db import models
 from applications.common.models import Cat001Estado, Cat004Ciudad
 
 # Create your models here.
-class SoftSkill(models.Model):
+class Cli053SoftSkill(models.Model):
     nombre = models.CharField(max_length=200)
-    estado = models.ForeignKey(Cat001Estado, on_delete=models.CASCADE)
+    estado_id_004 = models.ForeignKey(Cat001Estado, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
 
-class HardSkill(models.Model):
+class Cli054HardSkill(models.Model):
     nombre = models.CharField(max_length=200)
-    estado = models.ForeignKey(Cat001Estado, on_delete=models.CASCADE)
+    estado_id_004 = models.ForeignKey(Cat001Estado, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
     
-class ProfesionEstudio(models.Model):
+class Cli055ProfesionEstudio(models.Model):
     nombre = models.CharField(max_length=200)
-    estado = models.ForeignKey(Cat001Estado, on_delete=models.CASCADE)
+    estado_id_004 = models.ForeignKey(Cat001Estado, on_delete=models.CASCADE)
 
-class Vacante(models.Model):
+class Cli052Vacante(models.Model):
     ESTADO_VACANTE = [
-        (1, 'Abierto'),
-        (2, 'Cerrado'),
+        (1, 'Abierta'),
+        (2, 'Cerrada'),
+        (3, 'Cancelada'),
     ]
 
     titulo = models.CharField(max_length=200)
     numero_posiciones = models.IntegerField()
-    profesion_estudio = models.ForeignKey(ProfesionEstudio, on_delete=models.CASCADE)
+    profesion_estudio_id_055 = models.ForeignKey(Cli055ProfesionEstudio, on_delete=models.CASCADE)
     experiencia_requerida = models.TextField()
-    soft_skills = models.ManyToManyField(SoftSkill)
-    hard_skills = models.ManyToManyField(HardSkill)
+    soft_skills_id_053 = models.ManyToManyField(Cli053SoftSkill)
+    hard_skills_id_054 = models.ManyToManyField(Cli054HardSkill)
     funciones_responsabilidades = models.TextField()
     ciudad = models.ForeignKey(Cat004Ciudad, on_delete=models.CASCADE)
     salario = models.IntegerField(null=True, blank=True)  # Opcional
     estado_vacante = models.IntegerField(choices=ESTADO_VACANTE, default=1)
+    estado_id_004 = models.ForeignKey(Cat001Estado, models.DO_NOTHING)
 
     def __str__(self):
         return self.titulo
