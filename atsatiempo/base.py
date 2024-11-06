72ce9be0c0d5ac234e5a8671f66376e150a2458a
GUIDO ANGULO <sat1124@proton.me>
2024-11-06 16:28:30 -0500

    HUMANIZE EN BASE

--- atsatiempo/base.py
+++ atsatiempo/base.py
@@ -27,7 +27,7 @@ BASE_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django.contrib.humanize',
+    'django.contrib.humanize',
     'storages',
 ]
 
